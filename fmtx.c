cfd3a56928a6fcf6350009b30bf7c59f8bb279e9
Jacob Gelbman <gelbman@gmail.com>
2022-11-04 23:55:10 -0500

    Parse whole lines at a time

--- fmtx.c
+++ fmtx.c
@@ -8,29 +8,23 @@
 
 int width = 80;
 int center = 0;
-int input_word_count = 0;
-int output_word_count = 0;
-int output_width = 0;
 int paragraph_count = 0;
-int parsing_glyphs = 0;
-int continue_word = 0;
+int line_count = 0;
+int word_count = 0;
+int output_width = 0;
 
 #define BUFSIZE 1024
 int buf_len = 0;
 char buf[BUFSIZE];
-
-#define WORDSIZE 256
-int word_len = 0;
-char word[WORDSIZE];
 
 #define INDENTSIZE 256
 int indent_len = 0;
 int indent_width = 0;
 char indent[INDENTSIZE];
 
-#define LINESIZE 80
+int line_size = 256;
 int line_len = 0;
-char line[LINESIZE];
+char *line = NULL;
 
 void usage () {
     char str[] =
@@ -44,18 +38,38 @@ void usage () {
     exit(0);
 }
 
-void display_word () {
+void process_line_center () {
+    int trailing_ws_len = 0;
+    for (int i = 0; i < line_len; i++) {
+        char c = line[line_len - i - 1];
+        if (c == ' ' || c == '\t') {
+            trailing_ws_len++;
+        }
+        else {
+            break;
+        }
+    }
+    int trimmed_line_len = line_len - indent_len - trailing_ws_len;
+    if (trimmed_line_len > width) {
+        printf("%.*s\n", trimmed_line_len, line + indent_len);
+    }
+    else {
+        int center_indent_len = (width - trimmed_line_len) / 2;
+        printf("%*s%.*s\n", center_indent_len, "", trimmed_line_len, line + indent_len);
+    }
+}
+
+void process_word (char *word, int word_len) {
     if (indent_width >= width) {
-        if (output_word_count) {
+        if (word_count) {
             printf("\n");
         }
         printf("%.*s%.*s", indent_len, indent, word_len, word);
         output_width = indent_width + word_len;
-        output_word_count++;
         return;
     }
 
-    if (output_word_count == 0) {
+    if (word_count == 0) {
         printf("%.*s", indent_len, indent);
         output_width = indent_width;
     }
@@ -63,8 +77,7 @@ void display_word () {
         printf(" ");
         output_width++;
     }
-    else if (indent_width + word_len > width &&
-            output_width + 1 < width) {
+    else if (indent_width + word_len > width && output_width + 1 < width) {
         // the word can't fit on a line by itself
         // and at least one character can fit on the current line
         printf(" ");
@@ -79,10 +92,6 @@ void display_word () {
         printf("%.*s", word_len, word);
         output_width += word_len;
     }
-    else if (indent_width + word_len <= width) {
-        printf("%.*s", word_len, word);
-        output_width = indent_width + word_len;
-    }
     else {
         int remaining_len = word_len;
         int word_offset = 0;
@@ -96,162 +105,112 @@ void display_word () {
         printf("%.*s", remaining_len, word + word_offset);
         output_width += remaining_len;
     }
-    output_word_count++;
 }
 
-void set_indent () {
-    memcpy(indent, word, word_len);
-    indent_len = word_len;
-    paragraph_count++;
-    output_word_count = 0;
-    indent_width = 0;
-    for (int i = 0; i < indent_len; i++) {
-        char c = indent[i];
-        if (c == '\t') {
-            indent_width += 4;
+void process_line_wrap () {
+    int word_len = 0;
+    char *word = line + indent_len;
+    for (int i = indent_len; i < line_len; i++) {
+        char c = line[i];
+        if (c == ' ' || c == '\t') {
+            if (word_len) {
+                process_word(word, word_len);
+                word_count++;
+                word_len = 0;
+            }
         }
         else {
-            indent_width++;
+            if (!word_len) {
+                word = line + i;
+            }
+            word_len++;
         }
+    }
+    if (word_len) {
+        process_word(word, word_len);
+        word_count++;
+        word_len = 0;
     }
 }
 
-void process_buf_center () {
+void process_line () {
+    int cur_indent_len = 0;
+    int cur_indent_width = 0;
+    for (int i = 0; i < line_len; i++) {
+        char c = line[i];
+        if (c == ' ') {
+            cur_indent_len++;
+            cur_indent_width++;
+        }
+        else if (c == '\t') {
+            cur_indent_len++;
+            cur_indent_width += 4;
+        }
+        else {
+            break;
+        }
+    }
+    if (!center) {
+        if (line_count) {
+            if (cur_indent_len == line_len || cur_indent_len != indent_len || strncmp(indent, line, indent_len) != 0) {
+                // end paragraph
+                if (word_count) {
+                    printf("\n");
+                    word_count = 0;
+                    paragraph_count++;
+                }
+            }
+        }
+        if (cur_indent_len == line_len) {
+            printf("\n");
+        }
+    }
+    if (cur_indent_len > INDENTSIZE) {
+        fprintf(stderr, "Indent can't be larger than %d characters\n", INDENTSIZE);
+        exit(1);
+    }
+    memcpy(indent, line, cur_indent_len);
+    indent_len = cur_indent_len;
+    indent_width = cur_indent_width;
+    if (center) {
+        process_line_center();
+    }
+    else {
+        process_line_wrap();
+    }
+    line_count++;
+}
+
+void process_buf () {
     for (int i = 0; i < buf_len; i++) {
         char c = buf[i];
         if (c == '\n') {
-            if (line_len) {
-                if (line_len <= LINESIZE) {
-                    int indent_len = (width - line_len) / 2;
-                    printf("%*s%.*s\n", indent_len, "", line_len, line);
-                }
-                else {
-                    printf("\n");
-                }
-            }
-            else {
-                printf("\n");
-            }
-            input_word_count = 0;
-            parsing_glyphs = 0;
+            process_line();
             line_len = 0;
         }
-        else if (c == ' ' || c == '\t') {
-            if (parsing_glyphs) {
-                input_word_count++;
-                parsing_glyphs = 0;
-            }
-        }
         else {
-            if (!parsing_glyphs) {
-                input_word_count++;
-                parsing_glyphs = 1;
-                if (input_word_count > 1) {
-                    if (line_len < LINESIZE) {
-                        line[line_len] = ' ';
-                        line_len++;
-                    }
-                    else {
-                        if (line_len == LINESIZE) {
-                            printf("%.*s", line_len, line);
-                            line_len++;
-                        }
-                        printf(" ");
-                    }
-                }
+            if (line_len >= line_size) {
+                line_size *= 2;
+                line = realloc(line, line_size);
             }
-            if (line_len < LINESIZE) {
-                line[line_len] = c;
-                line_len++;
-            }
-            else {
-                if (line_len == LINESIZE) {
-                    printf("%.*s", line_len, line);
-                    line_len++;
-                }
-                printf("%c", c);
-            }
+            line[line_len++] = c;
         }
     }
-}
-
-void process_buf_wrap () {
-    for (int i = 0; i < buf_len; i++) {
-        char c = buf[i];
-        if (c == '\n') {
-            if (parsing_glyphs) {
-                display_word();
-                word_len = 0;
-                input_word_count++;
-            }
-            else if (input_word_count == 0) {
-                // line is just whitespace
-                printf("\n");
-                paragraph_count++;
-                output_word_count = 0;
-            }
-            input_word_count = 0;
-            parsing_glyphs = 0;
-        }
-        else if (c == ' ' || c == '\t') {
-            // "A" -> " "
-            if (parsing_glyphs) {
-                display_word();
-                word_len = 0;
-                input_word_count++;
-                parsing_glyphs = 0;
-            }
-            // * -> " "
-            // append the whitespace character to the current word
-            if (word_len == WORDSIZE) {
-                fprintf(stderr, "Can't be over %d consecutive whitespaces.\n", WORDSIZE);
-                exit(1);
-            }
-            word[word_len++] = c;
-        }
-        else {
-            // " " -> "A"
-            if (!parsing_glyphs) {
-                if (input_word_count == 0) {
-                    if (paragraph_count) {
-                        if (word_len == indent_len && strncmp(word, indent, word_len) == 0) {
-                            // this line is a part of the current paragraph
-                        }
-                        else {
-                            printf("\n");
-                            set_indent();
-                        }
-                    }
-                    else {
-                        set_indent();
-                    }
-                }
-                // you don't know if you want to print the whitespace because the
-                // following word might be moved to the next line, so the whitespace
-                // prior to a word is displayed when the visible word is displayed.
-                word_len = 0;
-                input_word_count++;
-                parsing_glyphs = 1;
-            }
-            // * -> "A"
-            if (word_len == WORDSIZE) {
-                // the word is too large for the buffer but we can show what
-                // we have and not show whitespace before next word
-                display_word();
-                continue_word = 1;
-                word_len = 0;
-                input_word_count++;
-            }
-            word[word_len++] = c;
-        }
+    if (line_len) {
+        process_line();
+    }
+    // end paragraph
+    if (word_count) {
+        printf("\n");
+        word_count = 0;
+        paragraph_count++;
     }
 }
 
 void process_file (int fd) {
-    input_word_count = 0;
-    output_word_count = 0;
+    word_count = 0;
     paragraph_count = 0;
-    parsing_glyphs = 0;
+    line_len = 0;
 
     while (1) {
         int retval = read(fd, buf, BUFSIZE);
@@ -260,19 +219,11 @@ void process_file (int fd) {
             break;
         }
         else if (retval == 0) {
-            if (output_word_count) {
-                printf("\n");
-            }
             break;
         }
         else {
             buf_len = retval;
-            if (center) {
-                process_buf_center();
-            }
-            else {
-                process_buf_wrap();
-            }
+            process_buf();
         }
     }
 
@@ -304,6 +255,7 @@ int main (int argc, char **argv) {
         }
     }
 
+    line = malloc(line_size);
     if (!isatty(STDIN_FILENO)) {
         process_file(STDIN_FILENO);
     }
@@ -323,9 +275,11 @@ int main (int argc, char **argv) {
 }
 
 // TODO
-// -w width
-// -c center
 // make sure if I have to break a word, multibyte characters arent broken up
 // -s convert indents to spaces if they contains tabs
-// parsing_glyphs to parsing_whitespace
-//
+// test when file doesn't end in newline
+// comments
+// bullets
+// numbered items
+// lettered items
+// roman numeraled items
