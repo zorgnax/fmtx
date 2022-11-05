4d5b350a993cbfaa5ccef75684a089973fdf027d
Jacob Gelbman <gelbman@gmail.com>
2022-11-05 16:16:54 -0500

    Respect comment starts when formatting

--- fmtx.c
+++ fmtx.c
@@ -9,9 +9,14 @@
 int width = 80;
 int center = 0;
 int paragraph_count = 0;
+int paragraph_line_count = 0;
 int line_count = 0;
 int word_count = 0;
 int output_width = 0;
+int line_start_len = 0;
+int line_start_width = 0;
+int line_start_full_width = 0;
+int max_indent_width = 0;
 
 #define BUFSIZE 1024
 int buf_len = 0;
@@ -22,9 +27,16 @@ int indent_len = 0;
 int indent_width = 0;
 char indent[INDENTSIZE];
 
+#define COMMENTSIZE 2
+int comment_len = 0;
+char comment[COMMENTSIZE];
+
 int line_size = 256;
 int line_len = 0;
 char *line = NULL;
+
+char *comments[] = {"//", "#"};
+int comments_count = sizeof(comments) / sizeof(comments[0]);
 
 void usage () {
     char str[] =
@@ -59,33 +71,45 @@ void process_line_center () {
     }
 }
 
+void print_prefix () {
+    output_width = 0;
+    if (indent_len) {
+        printf("%.*s", indent_len, indent);
+        output_width += indent_width;
+    }
+    if (comment_len) {
+        printf("%.*s ", comment_len, comment);
+        output_width += comment_len + 1;
+    }
+}
+
 void process_word (char *word, int word_len) {
-    if (indent_width >= width) {
+    if (line_start_full_width >= width) {
         if (word_count) {
             printf("\n");
         }
-        printf("%.*s%.*s", indent_len, indent, word_len, word);
-        output_width = indent_width + word_len;
+        print_prefix();
+        printf("%.*s", word_len, word);
+        output_width += word_len;
         return;
     }
 
     if (word_count == 0) {
-        printf("%.*s", indent_len, indent);
-        output_width = indent_width;
+        print_prefix();
     }
     else if (output_width + 1 + word_len <= width) {
         printf(" ");
         output_width++;
     }
-    else if (indent_width + word_len > width && output_width + 1 < width) {
+    else if (line_start_full_width + word_len > width && output_width + 1 < width) {
         // the word can't fit on a line by itself
         // and at least one character can fit on the current line
         printf(" ");
         output_width++;
     }
     else {
-        printf("\n%.*s", indent_len, indent);
-        output_width = indent_width;
+        printf("\n");
+        print_prefix();
     }
 
     if (output_width + word_len <= width) {
@@ -97,8 +121,8 @@ void process_word (char *word, int word_len) {
         int word_offset = 0;
         while (output_width + remaining_len > width) {
             int chunk_len = width - output_width;
-            printf("%.*s\n%.*s", chunk_len, word + word_offset, indent_len, indent);
-            output_width = indent_width;
+            printf("%.*s\n", chunk_len, word + word_offset);
+            print_prefix();
             word_offset += chunk_len;
             remaining_len -= chunk_len;
         }
@@ -109,8 +133,8 @@ void process_word (char *word, int word_len) {
 
 void process_line_wrap () {
     int word_len = 0;
-    char *word = line + indent_len;
-    for (int i = indent_len; i < line_len; i++) {
+    char *word = line + line_start_len;
+    for (int i = line_start_len; i < line_len; i++) {
         char c = line[i];
         if (c == ' ' || c == '\t') {
             if (word_len) {
@@ -133,6 +157,15 @@ void process_line_wrap () {
     }
 }
 
+void end_paragraph () {
+    if (word_count) {
+        printf("\n");
+        word_count = 0;
+        paragraph_count++;
+    }
+    paragraph_line_count = 0;
+}
+
 void process_line () {
     int cur_indent_len = 0;
     int cur_indent_width = 0;
@@ -150,28 +183,79 @@ void process_line () {
             break;
         }
     }
+    line_start_len = cur_indent_len;
+    line_start_width = cur_indent_width;
+
+    char *cur_comment = line + cur_indent_len;
+    int cur_comment_len = 0;
+
+    for (int i = 0; i < comments_count; i++) {
+        char *pcomment = comments[i];
+        int len = strlen(pcomment);
+        if (line_len - line_start_len >= len && strncmp(cur_comment, pcomment, len) == 0) {
+            cur_comment_len = len;
+            break;
+        }
+    }
+    line_start_len += cur_comment_len;
+    line_start_width += cur_comment_len;
+
+    // Lines that start with a comment get an extra space before
+    // the content of the line.
+    line_start_full_width = line_start_width;
+    if (cur_comment_len) {
+        line_start_full_width += 1;
+    }
+
     if (!center) {
         if (line_count) {
-            if (cur_indent_len == line_len || cur_indent_len != indent_len || strncmp(indent, line, indent_len) != 0) {
-                // end paragraph
-                if (word_count) {
-                    printf("\n");
-                    word_count = 0;
-                    paragraph_count++;
-                }
+            // if it's an empty line
+            if (cur_indent_len == line_len) {
+                end_paragraph();
+            }
+
+            // if the indents don't match
+            else if (cur_indent_len < indent_len || strncmp(indent, line, indent_len) != 0) {
+                end_paragraph();
+            }
+
+            // if the indent is larger than the allowed maximum the indent can be, for
+            // example a line that starts with:
+            //     - a list item
+            //       more text
+            // the "more text" part could be aligned with the "-" or the "a"
+            else if (cur_indent_width > max_indent_width) {
+                end_paragraph();
+            }
+
+            // if the comment starts don't match
+            else if (cur_comment_len != comment_len || strncmp(comment, cur_comment, comment_len) != 0) {
+                end_paragraph();
             }
         }
         if (cur_indent_len == line_len) {
             printf("\n");
         }
     }
-    if (cur_indent_len > INDENTSIZE) {
-        fprintf(stderr, "Indent can't be larger than %d characters\n", INDENTSIZE);
-        exit(1);
+
+    if (paragraph_line_count == 0) {
+        max_indent_width = cur_indent_width;
+        if (cur_indent_len > INDENTSIZE) {
+            fprintf(stderr, "Indent can't be larger than %d characters\n", INDENTSIZE);
+            exit(1);
+        }
+        memcpy(indent, line, cur_indent_len);
+        indent_len = cur_indent_len;
+        indent_width = cur_indent_width;
+
+        if (cur_comment_len > COMMENTSIZE) {
+            fprintf(stderr, "Comment start can't be larger than %d characters\n", COMMENTSIZE);
+            exit(1);
+        }
+        memcpy(comment, cur_comment, cur_comment_len);
+        comment_len = cur_comment_len;
     }
-    memcpy(indent, line, cur_indent_len);
-    indent_len = cur_indent_len;
-    indent_width = cur_indent_width;
+
     if (center) {
         process_line_center();
     }
@@ -179,6 +263,7 @@ void process_line () {
         process_line_wrap();
     }
     line_count++;
+    paragraph_line_count++;
 }
 
 void process_buf () {
@@ -199,18 +284,14 @@ void process_buf () {
     if (line_len) {
         process_line();
     }
-    // end paragraph
-    if (word_count) {
-        printf("\n");
-        word_count = 0;
-        paragraph_count++;
-    }
+    end_paragraph();
 }
 
 void process_file (int fd) {
     word_count = 0;
     paragraph_count = 0;
     line_len = 0;
+    indent_len = 0;
 
     while (1) {
         int retval = read(fd, buf, BUFSIZE);
@@ -277,9 +358,8 @@ int main (int argc, char **argv) {
 // TODO
 // make sure if I have to break a word, multibyte characters arent broken up
 // -s convert indents to spaces if they contains tabs
-// test when file doesn't end in newline
-// comments
 // bullets
 // numbered items
 // lettered items
 // roman numeraled items
+
